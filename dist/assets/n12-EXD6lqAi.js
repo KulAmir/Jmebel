35b28ca9cdae36de72f938c60f13430e5968eb22
amir <kz.amir_kulzhanov@mail.ru>
2024-06-21 18:14:23 +0500

    img

--- dist/assets/n12-EXD6lqAi.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/n5-By6z3Gi7.jpg",t="/assets/n4-Nq58zdGN.jpg",o="/assets/n12-CACo-Wxp.jpg";export{s as _,t as a,o as b};
---
+++ dist/assets/n12-KH45I2Ou.js
@@ -0,0 +1,1 @@
+const o="/img-logo/Gallere_neo/n5.jpg",e="/img-logo/Gallere_neo/n4.jpg",_="/img-logo/Gallere_neo/n12.jpg";export{o as _,e as a,_ as b};
